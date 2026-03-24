4c3fbc055f656ab8b592b4932b826bc6186f6174
Caleb Brown <calebnbrown2004@gmail.com>
2026-03-24 13:21:19 -0500

    add tmuxp and zshrc

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,3 +1,15 @@
 require("caleb")
 
+vim.o.autoread = true
+vim.o.updatetime = 250
+local cursor_file_watch = vim.api.nvim_create_augroup("CursorFileWatch", { clear = true })
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+  group = cursor_file_watch,
+  pattern = "*",
+  callback = function()
+    if vim.fn.mode() ~= "c" then
+      vim.cmd("checktime")
+    end
+  end,
+})
 vim.cmd("colorscheme dawnfox")
